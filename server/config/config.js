e78fbddc9721d870463f169050d195730d92ae15
kauvia <mkhw1989@gmail.com>
2019-03-07 23:39:50 +0800

    fixing routes

--- server/config/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    secret: 'antsforlunch',
-    consumerKeyT: 'uj0Dzi7sf6WpZJjvVpeGgLiFO',
-    consumerSecretT: 'aEgbnCj3zjeczdAPvf3YEhVnClG9Nt4ULSwCvZc9jPodg16rFr',
-    accessTokenT:'1082159017680461825-k8pszztLZIOAfuoGUmcdXOVjD0YAWM',
-    accessSecretT:'7p6wcj5vRNkigBMPRdFvXhoLf2wEVqoT8ANO3GhOWOfuz',
-    googleAPI:'6ec2b39e5de04e6a85e28f688d6eb2cb'
-}
